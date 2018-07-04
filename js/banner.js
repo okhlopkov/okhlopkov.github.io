415272267b33a782b277bb89ab1beaa182a30ff3
Dannil <danokhlopkov@gmail.com>
2018-07-05 00:03:51 +0300

    banner changer js

---
+++ js/banner.js
@@ -0,0 +1,14 @@
+function randomBanner(){
+  var banners = ["url('../img/banners/1.jpg')",
+                  "url('../img/banners/2.jpg')",
+                  "url('../img/banners/3.jpg')",
+                  "url('../img/banners/4.jpg')",
+                  "url('../img/banners/5.jpg')",
+                ];
+
+  setInterval(function() {
+    var random = Math.floor(Math.random() * banners.length + 0);
+    console.log(random);
+    document.getElementById('banner').style.backgroundImage=banners[random];
+  }, 10000)
+}
